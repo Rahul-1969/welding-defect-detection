ebf2c605c3e1a36e0be6f78a5ec663d70888d0b0
Rahul-1969 <kusumanirahul@gmail.com>
2026-03-15 20:23:51 +0530

    auto download model from google drive

--- webapp/model_loader.py
+++ webapp/model_loader.py
@@ -1,9 +1,25 @@
 import torch
 import torchvision
+import os
+import gdown
 
-device="cpu"
+device = "cpu"
 
-num_classes=4
+num_classes = 4
+
+MODEL_PATH = "models/weld_final.pth"
+
+# download model if not present
+if not os.path.exists(MODEL_PATH):
+
+    os.makedirs("models", exist_ok=True)
+
+    print("Downloading model from Google Drive...")
+
+    url = "https://drive.google.com/uc?id=1t20qdH6Jc4Zd2-sDrDndg-nD3usui5KU"
+
+    gdown.download(url, MODEL_PATH, quiet=False)
+
 
 model = torchvision.models.detection.retinanet_resnet50_fpn(weights=None)
 
@@ -16,7 +32,7 @@ model.head.classification_head = torchvision.models.detection.retinanet.RetinaNe
     num_classes
 )
 
-model.load_state_dict(torch.load("models/weld_final.pth",map_location=device))
+model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
 
 model.to(device)
 model.eval()
